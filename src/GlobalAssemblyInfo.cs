f448071efb7bd9558f52ae3958ba8caa7564d648
Darius Damalakas <darius.damalakas@gmail.com>
2016-06-03 10:36:08 +0100

    Adding GlobalAssemblyInfo

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+//
+//     Changes to this file may cause incorrect behavior and will be lost if
+//     the code is regenerated.
+// </auto-generated>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("RingCentral ")]
+[assembly: AssemblyProduct("RingCentral SDK")]
+[assembly: AssemblyCopyright("RingCentral")]
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyVersion("0.2.100")]
+[assembly: AssemblyFileVersion("0.2.100")]
+[assembly: AssemblyInformationalVersion("0.2.100.044b5c90e3422204b5c576dfa2db4359dde0d65")]
