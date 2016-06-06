eb4530b197379398437eecb66cca0b12f378163a
Darius Damalakas <darius.damalakas@gmail.com>
2016-06-06 10:01:19 +0100

    Adding request/resposne properties to ApiException

--- RingCentral/Http/ApiException.cs
+++ RingCentral/Http/ApiException.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace RingCentral.Http
 {
     public class ApiException : Exception
     {
+        public HttpResponseMessage Response { get; }
+        public HttpRequestMessage Request { get; }
+
         public ApiException(string message) : base(message)
         {
+        }
+
+        public ApiException(string message, HttpResponseMessage response, HttpRequestMessage request) : base(message)
+        {
+            this.Response = response;
+            this.Request = request;
         }
     }
 }
